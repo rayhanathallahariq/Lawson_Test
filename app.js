d9d813f31e7f3d39a0b23090fe5b8f06250d2d7a
rayhanathallahariq <108610498+rayhanathallahariq@users.noreply.github.com>
2023-08-16 12:47:43 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const cors = require('cors')
+const express = require('express')
+const app = express()
+const port = 3000
+const router = require("./routers")
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+app.use('/', router)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
